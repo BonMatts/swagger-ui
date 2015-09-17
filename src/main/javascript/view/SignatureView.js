edbfb52ebbc4e6803aeade57f8240766f42aef87
arno-di-loreto <arno.di.loreto@gmail.com>
2015-09-17 14:41:11 -0400

    Add hypermedia panel to response

--- src/main/javascript/view/SignatureView.js
+++ src/main/javascript/view/SignatureView.js
@@ -4,6 +4,7 @@ SwaggerUi.Views.SignatureView = Backbone.View.extend({
   events: {
     'click a.description-link'       : 'switchToDescription',
     'click a.snippet-link'           : 'switchToSnippet',
+    'click a.hypermedia-link'           : 'switchToHypermedia',
     'mousedown .snippet'          : 'snippetToTextArea'
   },
 
@@ -32,7 +33,21 @@ SwaggerUi.Views.SignatureView = Backbone.View.extend({
 
     $('.snippet', $(this.el)).hide();
     $('.description', $(this.el)).show();
+    $('.hypermedia', $(this.el)).hide();
     $('.description-link', $(this.el)).addClass('selected');
+    $('.snippet-link', $(this.el)).removeClass('selected');
+    $('.hypermedia-link', $(this.el)).removeClass('selected');
+  },
+
+  // handler for show signature
+  switchToHypermedia: function(e){
+    if (e) { e.preventDefault(); }
+
+    $('.snippet', $(this.el)).hide();
+    $('.description', $(this.el)).hide();
+    $('.hypermedia', $(this.el)).show();
+    $('.hypermedia-link', $(this.el)).addClass('selected');
+    $('.description-link', $(this.el)).removeClass('selected');
     $('.snippet-link', $(this.el)).removeClass('selected');
   },
 
@@ -42,8 +57,10 @@ SwaggerUi.Views.SignatureView = Backbone.View.extend({
 
     $('.description', $(this.el)).hide();
     $('.snippet', $(this.el)).show();
+    $('.hypermedia', $(this.el)).hide();
     $('.snippet-link', $(this.el)).addClass('selected');
     $('.description-link', $(this.el)).removeClass('selected');
+    $('.hypermedia-link', $(this.el)).removeClass('selected');
   },
 
   // handler for snippet to text area
